c68bb412c4ce20cb01d13b3cf3a96878e1a70932
Azeliah <71652004+Azeliah@users.noreply.github.com>
2022-03-05 09:56:50 +0100

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,14 @@
+"""
+Initial python code.
+Example of commenting
+"""
+
+main():
+    message1 = 'Hello'
+    message2 = 'World!'
+    # String concatenation in Python
+    print(message1 + ' ' + message2)
+
+
+if __name__ == "__main__":
+    main()
