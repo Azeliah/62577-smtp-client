53c3ab4ef77e3db93c297bac433e7b1b72153a98
Azeliah <s205460@student.dtu.dk>
2022-03-12 12:48:32 +0100

    MIME implemented. Picture attachment added (working). Previous code commented out.

--- __main__.py
+++ __main__.py
@@ -2,35 +2,100 @@
 Initial python code.
 Example of commenting
 """
-import socket
 from socket import *
-import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+
+def get_mail_strings():
+    prompts = ["Sender email (TA email): ",
+               "Recipient email (student email): ",
+               "Message subject: ",
+               "Message body (finish with a separate line with a single period): "]
+
+    inputs = []
+
+    for i in range(len(prompts)):
+        print(prompts[i])
+        if i == 3:
+            while inputs[len(inputs) - 1] != ".":
+                input1 = str(input())
+                """
+                if input1 == ".":)
+                    inputs.append('.')
+                """
+                inputs.append(input1)
+
+
+        else:
+            inputs.append(str(input()))  # TODO: Add some input validation here.
+
+    return inputs
+
+
+def string_split(text, delimiter):
+    return text.split(delimiter, len(text))
+
+
+def mime_message():
+    img_filepath = 'verification_screenshot.jpg'
+    with open(img_filepath, 'rb') as f:
+        img_data = f.read()
+
+    strings = get_mail_strings()
+    msg = MIMEMultipart()
+    msg['From'] = strings[0]
+    msg['To'] = strings[1]
+    msg['Subject'] = strings[2]
+
+    jpg_part = MIMEApplication(img_data)
+    jpg_part.add_header('Content-Disposition', 'attachment', filename='proof.jpg')
+    msg.attach(jpg_part)
+
+    msg_body = ''
+    for i in range(len(strings) - 3):
+        if i != len(strings) - 3:
+            msg_body = msg_body + strings[i + 3] + '\r\n\0'
+        else:
+            msg_body = msg_body + strings[i + 3] + '\r\n'
+
+    text = MIMEText(msg_body)
+    msg.attach(text)
+
+    return msg
+
 
 def main():
+    # strings = get_mail_strings()
+    msg = mime_message()
+
     mail_server = 'smtp2.bhsi.xyz'
     server_port = 2525
 
+    line_terminator = '\r\n'
+
     # Mail content
-    ta_email = 's171242@student.dtu.dk'
-    rcpt_email = 's205460@dtu.dk'
-
-
     """
-    current_time = datetime.datetime.now()
-    print(current_time.strftime("%Y-%m-%d %H:%M:%S"))
-    print(current_time)
+    ta_email = strings[0]  # 's171242@student.dtu.dk'
+    rcpt_email = strings[1]
+    subject = strings[2]
+    mail_header = ['From: ' + ta_email + '\r\n',
+                   'To: ' + rcpt_email + '\r\n',
+                   'Subject:' + subject + '\r\n',
+                   '\r\n']
+
+    mail_body = []
+
+    for i in range(len(strings) - 3):
+        print(i)
+        mail_body.append(strings[i + 3])
+        mail_body.append(line_terminator)
     """
 
-    mail_body = [
-        "From: " + ta_email + "\r\n",
-        "To: " + rcpt_email + "\r\n",
-        "Subject: Whatever man vælger\r\n",
-        "\r\n",
-        "Tillykke med din nysendte email fra din TA.\r\n",
-        "Hurra.\r\n",
-        ".\r\n"
-    ]
+    msg_lines = string_split(msg.as_string(), '\0')
 
+    print(msg_lines)
     # Establish TCP connection to mail_server
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect((mail_server, server_port))
@@ -43,7 +108,7 @@ def main():
         client_socket.close()
         return
 
-    helo_command = 'HELO Alice\r\n'
+    helo_command = 'EHLO Notalice\r\n'
 
     client_socket.send(helo_command.encode())
 
@@ -57,7 +122,7 @@ def main():
 
     print('SMTP connection successfully established.')
 
-    mail_from_command = 'MAIL FROM:<' + ta_email + '>\r\n'
+    mail_from_command = 'MAIL FROM:<' + msg['From'] + '>\r\n'
 
     client_socket.send(mail_from_command.encode())
 
@@ -69,7 +134,7 @@ def main():
         client_socket.close()
         return
 
-    rcpt_command = 'RCPT TO:<' + rcpt_email + '>\r\n'
+    rcpt_command = 'RCPT TO:<' + msg['To'] + '>\r\n'
 
     client_socket.send(rcpt_command.encode())
 
@@ -91,8 +156,16 @@ def main():
         client_socket.close()
         return
 
+    for line in msg_lines:
+        client_socket.send(line.encode())
+
+    """
+    for line in mail_header:
+        client_socket.send(line.encode())
+
     for line in mail_body:
         client_socket.send(line.encode())
+    """
 
     receive = client_socket.recv(1024).decode()
 
@@ -102,7 +175,7 @@ def main():
         return
 
     print("Mail body successfully sent to mailserver.")
-
+    """
     client_socket.send('QUIT\r\n'.encode())
 
     receive = client_socket.recv(1024).decode()
@@ -111,7 +184,7 @@ def main():
         print('221 reply not received from server.')
         client_socket.close()
         return
-
+    """
     print("Ended session with mailserver, closing socket.")
 
     client_socket.close()
